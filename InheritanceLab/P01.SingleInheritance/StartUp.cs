791eabb869861214951e6fb76d2f4578f285c7d0
Andrian Dimitrov <andihacera@abv.bg>
2022-07-04 13:00:39 +0300

    First 2 problems from inheritance lab are done

---
+++ InheritanceLab/P01.SingleInheritance/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ////P01
+            //Dog dog = new Dog();
+            //dog.Eat();
+            //dog.Bark();
+
+            ////P02
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}

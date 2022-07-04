19003b31d23af2456698ab5d7aa99f5874471c66
Andrian Dimitrov <andihacera@abv.bg>
2022-07-04 13:32:53 +0300

    last problem done

---
+++ InheritanceLab/P05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings:Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count==0;
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var el in collection)
+                this.Push(el);
+        }
+    }
+}
